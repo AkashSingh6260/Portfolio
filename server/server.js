a4d7d813b00daff5d17d74cdf2459f4ef8d7cec2
Akash Singh <akki.akashsingh2005@gmail.com>
2026-05-16 19:22:58 +0530

    Force Nodemailer SMTP connection to IPv4 on Render

--- server/server.js
+++ server/server.js
@@ -40,6 +40,7 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
   secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER, // Your email address
     pass: process.env.EMAIL_PASS, // Your App Password
